18646bced92de77a6d2f1fa65846a49dfedcf8b6
Khalil Carsten <khalilcarsten@gmail.com>
2018-05-15 14:40:15 -0300

    2 bibliotecas de leittura de metados adicionadas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate exif;
+extern crate rexiv2;
+
 fn main() {
-    println!("Hello, world!");
+
+    // O codigo descomentado eh relativo ao rexiv2
+
+    let file = "images/andre.jpg";
+
+    // Essa Tag que diferencia em quais dados ele pega
+    let tag = "Iptc.Application2.Keywords";
+    let meta = rexiv2::Metadata::new_from_path(&file).unwrap();
+    println!("{:?}", meta.get_tag_multiple_strings(tag));
+
+    // Código comentado abaixo eh relativo ao exif
+
+    // let file = std::fs::File::open("images/andre.jpg").unwrap();
+    // let reader = exif::Reader::new(
+    //     &mut std::io::BufReader::new(&file)).unwrap();
+    //
+    // for f in reader.fields() {
+    //     println!("{} {} {:?}", f.tag, f.thumbnail, f.value);
+    // }
 }
