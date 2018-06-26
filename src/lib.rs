cc505edeeeaef5dacd3b3b4fc055632eec213a2f
Kevin Souto <soutokevin@gmail.com>
2018-06-26 10:54:41 -0300

    format code with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 #![feature(proc_macro, wasm_custom_section, wasm_import_module)]
 
-extern crate wasm_bindgen;
 extern crate exif;
+extern crate wasm_bindgen;
 
+use exif::Value;
 use wasm_bindgen::prelude::*;
-use exif::{Value};
 
 #[wasm_bindgen]
 pub fn read_file(vec: &[u8]) -> String {
@@ -41,8 +41,7 @@ pub fn read_file(vec: &[u8]) -> String {
 
 #[wasm_bindgen]
 pub fn read_metadata(vec: &[u8]) -> Vec<f64> {
-    let reader = exif::Reader::new(
-    &mut std::io::BufReader::new(vec)).unwrap();
+    let reader = exif::Reader::new(&mut std::io::BufReader::new(vec)).unwrap();
 
     let deegres_lati;
     let min_lati;
@@ -52,48 +51,63 @@ pub fn read_metadata(vec: &[u8]) -> Vec<f64> {
     let min_long;
     let sec_long;
 
-    match reader.get_field(exif::Tag::GPSLatitude, false).unwrap().value {
+    match reader
+        .get_field(exif::Tag::GPSLatitude, false)
+        .unwrap()
+        .value
+    {
         Value::Rational(ref lati_cord) => {
             deegres_lati = lati_cord[0].to_f64();
             min_lati = lati_cord[1].to_f64();
             sec_lati = lati_cord[2].to_f64();
         }
-        _ => panic!()
+        _ => panic!(),
     }
 
-    match reader.get_field(exif::Tag::GPSLongitude, false).unwrap().value {
+    match reader
+        .get_field(exif::Tag::GPSLongitude, false)
+        .unwrap()
+        .value
+    {
         Value::Rational(ref long_cord) => {
             deegres_long = long_cord[0].to_f64();
             min_long = long_cord[1].to_f64();
             sec_long = long_cord[2].to_f64();
         }
-        _ => panic!()
+        _ => panic!(),
     }
 
-    let mut latitude = deegres_lati + (min_lati/60.0) + (sec_lati/3600.0);
-    let mut longitude = deegres_long + (min_long/60.0) + (sec_long/3600.0);
+    let mut latitude = deegres_lati + (min_lati / 60.0) + (sec_lati / 3600.0);
+    let mut longitude = deegres_long + (min_long / 60.0) + (sec_long / 3600.0);
 
-    match reader.get_field(exif::Tag::GPSLatitudeRef, false).unwrap().value {
+    match reader
+        .get_field(exif::Tag::GPSLatitudeRef, false)
+        .unwrap()
+        .value
+    {
         Value::Ascii(ref referencia) => {
             if referencia[0][0] == b'S' {
                 latitude *= -1.0;
             }
         }
-        _ => panic!()
+        _ => panic!(),
     }
 
-    match reader.get_field(exif::Tag::GPSLongitudeRef, false).unwrap().value {
+    match reader
+        .get_field(exif::Tag::GPSLongitudeRef, false)
+        .unwrap()
+        .value
+    {
         Value::Ascii(ref referencia) => {
             if referencia[0][0] == b'W' {
                 longitude *= -1.0;
             }
         }
-        _ => panic!()
+        _ => panic!(),
     }
 
     println!("latitude {:?}", latitude);
     println!("Longitude {:?}", longitude);
 
-    vec!(latitude, longitude)
-
+    vec![latitude, longitude]
 }
